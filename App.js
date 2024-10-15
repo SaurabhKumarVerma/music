906bdf1fc9b762d507939a227e73c295fa1d1b49
Saurabh Kumar <saurabh@Saurabhs-MacBook-Air.local>
2024-10-15 22:25:33 +0530

    change es-lint file

--- App.js
+++ App.js
@@ -1,84 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text } from 'react-native';
+import { StatusBar } from "expo-status-bar"
+import { StyleSheet, Text } from "react-native"
 
-import * as Splash from 'expo-splash-screen';
-import { Provider } from 'react-redux';
-import * as WebBrowser from 'expo-web-browser';
-import { NavigationContainer } from '@react-navigation/native';
-import { store } from '@music/store/store';
-import SplashScreen from '@music/screen/SplashScreen';
-import FloatingScreen from '@music/screen/FloatingScreen/FloatingScreen';
-import { navigationRef } from '@music/navigation/Rootnavigation';
-import { BOTTOM_BAR_HEIGHT } from '@music/constant/constant';
-import RootNavigator from '@music/navigation/RootNavigator';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { createIconSetFromIcoMoon } from '@expo/vector-icons';
-import { useFonts } from 'expo-font';
-import { useEffect } from 'react';
-import * as Linking from 'expo-linking';
+import * as Splash from "expo-splash-screen"
+import { Provider } from "react-redux"
+import * as WebBrowser from "expo-web-browser"
+import { NavigationContainer } from "@react-navigation/native"
+import { store } from "@music/store/store"
+import SplashScreen from "@music/screen/SplashScreen"
+import FloatingScreen from "@music/screen/FloatingScreen/FloatingScreen"
+import { navigationRef } from "@music/navigation/Rootnavigation"
+import { BOTTOM_BAR_HEIGHT } from "@music/constant/constant"
+import RootNavigator from "@music/navigation/RootNavigator"
+import { GestureHandlerRootView } from "react-native-gesture-handler"
+import { createIconSetFromIcoMoon } from "@expo/vector-icons"
+import { useFonts } from "expo-font"
+import { useEffect } from "react"
+import * as Linking from "expo-linking"
 
-
-Splash.preventAutoHideAsync();
+Splash.preventAutoHideAsync()
 export const Icon = createIconSetFromIcoMoon(
   require("./assets/icons/selection.json"),
   "IcoMoon",
-  "icomoon.ttf"
-);
-WebBrowser.maybeCompleteAuthSession();
+  "icomoon.ttf",
+)
+WebBrowser.maybeCompleteAuthSession()
 export default function App() {
-  // const progress = useProgress();
   const [loaded, error] = useFonts({
     IcoMoon: require("./assets/icons/icomoon.ttf"),
-  });
+  })
 
   useEffect(() => {
     if (loaded || error) {
-      Splash.hideAsync();
+      Splash.hideAsync()
     }
-  }, [loaded, error]);
+  }, [loaded, error])
 
   if (!loaded && !error && !navigationRef.isReady) {
-    return null;
+    return null
   }
 
   const linking = {
-    prefixes: [Linking.createURL('/'), "music://auth/spotify", "exp://localhost:8081/--/spotify-auth-callback", "myapp://"],
+    prefixes: [
+      Linking.createURL("/"),
+      "music://auth/spotify",
+      "exp://localhost:8081/--/spotify-auth-callback",
+      "myapp://",
+    ],
     config: {
       // main: {
-        initialRouteName: 'main',
-        screens: {
-          Home: 'home',
-          Setting: 'setting',
-          Search: 'search'
-        }
-      }
+      initialRouteName: "main",
+      screens: {
+        Home: "home",
+        Setting: "setting",
+        Search: "search",
+      },
+    },
     // }
-  };
+  }
 
   // tokenCache={tokenCache}
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
+    <GestureHandlerRootView style={{ flex: 1, backgroundColor: "#131212" }}>
       <Provider store={store}>
-            <NavigationContainer ref={navigationRef} linking={linking} fallback={<Text>Loading...</Text>}>
-            <SplashScreen />
-              <RootNavigator />
-              <StatusBar style="auto" />
-              <FloatingScreen style={styles.floatingScreen} />
-            </NavigationContainer>
+        <NavigationContainer
+          ref={navigationRef}
+          linking={linking}
+          fallback={<Text>Loading...</Text>}
+        >
+          <SplashScreen />
+          <RootNavigator />
+          <StatusBar style="auto" />
+          <FloatingScreen style={styles.floatingScreen} />
+        </NavigationContainer>
       </Provider>
     </GestureHandlerRootView>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    // flex: 1,
-    backgroundColor: '#131212',
-  },
   floatingScreen: {
-    position: 'absolute',
-    left: 6,
-    right: 6,
     bottom: BOTTOM_BAR_HEIGHT + 2,
-  }
-});
+    left: 6,
+    position: "absolute",
+    right: 6,
+  },
+})
