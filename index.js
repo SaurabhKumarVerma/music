b62f7e5a1026922fcd63bb038630b9b714174df8
Saurabh Kumar <saurabh@MacBookAir.lan>
2024-12-16 02:59:49 +0530

    un commented

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import "react-native-gesture-handler"
 import "react-native-url-polyfill/auto"
 import App from "./App"
 import { AppRegistry } from "react-native"
+import TrackPlayer from "react-native-track-player"
+import { PlaybackService } from "./src/service/audio/PlaybackService"
 
 // registerRootComponent(appName, () => App);
-// TrackPlayer.registerPlaybackService(() => PlaybackService);
 AppRegistry.registerComponent("main", () => App)
+TrackPlayer.registerPlaybackService(() => PlaybackService)
