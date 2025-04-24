ebe4246b7f24cc17762ae195ac98f2195f1f96e7
Saurabh Kumar <saurabh@MacBookAir.lan>
2025-04-25 03:01:58 +0530

    Add Lottie animation for artist popularity and refactor MusicImage component to use forwardRef

--- src/base/MusicImage/MusicImage.tsx
+++ src/base/MusicImage/MusicImage.tsx
@@ -1,11 +1,12 @@
+import React from "react"
 import { Image } from "expo-image"
 import { IMusicImage } from "@music/types/image.interface"
 
-const MusicImage = (props: IMusicImage) => {
+const MusicImage = React.forwardRef<Image, IMusicImage>((props, ref) => {
   // const {placeholder, ...rest} = props
   const blurhash =
     "|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj["
-  return <Image {...props} cachePolicy={"memory"} transition={2000} />
-}
+  return <Image ref={ref} {...props} cachePolicy={"memory"} transition={2000} />
+})
 
 export default MusicImage
--- src/component/Artist/Artist.tsx
+++ src/component/Artist/Artist.tsx
@@ -1,7 +1,8 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import {
   NativeScrollEvent,
   NativeSyntheticEvent,
-  NativeUIEvent,
+  Platform,
   Pressable,
   ScrollView,
   StyleSheet,
@@ -12,25 +13,97 @@ import { useNavigation, useRoute } from "@react-navigation/native"
 import { useAppDispatch, useAppSelector } from "@music/hook/hook"
 import MusicImage from "@music/base/MusicImage/MusicImage"
 import Loading from "@music/base/Loading/Loading"
-import { DEVICE_WIDTH } from "@music/constant/constant"
+import { DEVICE_HEIGHT, DEVICE_WIDTH } from "@music/constant/constant"
 import { artistData } from "@music/store/slice/artistSlice"
 import { MusicText } from "@music/base/MusicText/MusicText"
 import { AntDesign, SimpleLineIcons } from "@expo/vector-icons"
 import { color } from "@music/theme/color"
-import Animated, { FadeInLeft, useAnimatedStyle, useSharedValue } from "react-native-reanimated"
+import Animated, {
+  FadeInLeft,
+  interpolate,
+  useAnimatedScrollHandler,
+  useAnimatedStyle,
+  useDerivedValue,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated"
 import { useSafeAreaInsets } from "react-native-safe-area-context"
+import { LinearGradient } from "expo-linear-gradient"
+import { images } from "assets"
+import { usePlayerBackground } from "@music/hook/usePlayerBackground"
+import LottieView from "lottie-react-native"
+import { Gesture, GestureDetector } from "react-native-gesture-handler"
+
+const AnimatedLinearGradient = Animated.createAnimatedComponent(LinearGradient)
+const AnimatedImage = Animated.createAnimatedComponent(MusicImage)
+const AnimatedPressable = Animated.createAnimatedComponent(Pressable)
 
 const Artist = () => {
   const route = useRoute()
   const dispatched = useAppDispatch()
-  const translateY = useSharedValue(0)
+  const translateY = useSharedValue(4)
+  const height = useSharedValue(0)
+
   const ref = useRef<ScrollView>(null)
   const inset = useSafeAreaInsets()
   const navigation = useNavigation()
   const { artist, isArtistError, isArtistLoading } = useAppSelector((state) => state.artist)
+
+  const { imageColors } = usePlayerBackground(
+    artist[0]?.data?.images[1]?.url || images.variousArtisit1,
+  )
+
   useEffect(() => {
     dispatched(artistData((route.params as { artistId: string }).artistId))
   }, [])
+
+  const onScroll = useAnimatedScrollHandler({
+    onScroll: (event) => {
+      translateY.value = event.contentOffset.y
+    },
+  })
+
+  const opacityStyle = useAnimatedStyle(() => {
+    const opacity = interpolate(
+      translateY.value,
+      [80, 90, 100, 120, 140, 160, 200, 220, 240],
+      [1, 0.5, 0.5, 0.4, 0.2, 0.1, 0.1, 0, 0],
+      "clamp",
+    )
+
+    return {
+      opacity,
+      // width,
+      // height,
+      // aspectRatio,
+    }
+  })
+
+  const animatedImageHeight = useAnimatedStyle(() => {
+    const height = interpolate(translateY.value, [0, 100, 200], [200, 100, 0], "clamp")
+    const width = interpolate(translateY.value, [0, 100, 200], [200, 100, 0], "clamp")
+
+    return {
+      height,
+      width,
+    }
+  })
+
+  const animatedTextContainer = useAnimatedStyle(() => {
+    const opacity = interpolate(translateY.value, [0, 100, 150, 200], [1, 0.4, 0.3, 0], "clamp")
+
+    return {
+      opacity,
+    }
+  })
+
+  const animatedPressableContainer = useAnimatedStyle(() => {
+    const opacity = interpolate(translateY.value, [0, 10, 12, 20], [1, 0.2, 0.1, 0], "clamp")
+
+    return {
+      opacity,
+    }
+  })
 
   if (isArtistLoading && artist !== undefined) {
     return (
@@ -42,11 +115,44 @@ const Artist = () => {
 
   const header = () => {
     return (
-      <MusicImage
-        source={{ uri: artist[0]?.data?.images[1]?.url }}
-        style={styles.authorImageStyle}
-        contentFit="cover"
-      />
+      <Animated.View>
+        <AnimatedImage
+          source={{ uri: artist[0]?.data?.images[1]?.url }}
+          style={[styles.authorImageStyle, animatedImageHeight]}
+          contentFit="cover"
+        />
+        <Animated.View>
+          {artist[0]?.data?.images[1]?.url ? (
+            <Animated.View entering={FadeInLeft.delay(2000)} style={{ marginTop: 20 }}>
+              <MusicText
+                numberOfLines={3}
+                text={artist[0]?.data?.name}
+                style={[styles.textStyle, { flexShrink: 1, justifyContent: "flex-start" }]}
+                preset="semiBold"
+                size="lg"
+              />
+            </Animated.View>
+          ) : null}
+
+          <Animated.View style={[styles.popularityContainer, animatedTextContainer]}>
+            <View style={styles.popularityContainer}>
+              <LottieView
+                source={require("../../../assets/lottie/active.json")}
+                style={{ height: 50, width: 50 }}
+                autoPlay={true}
+                loop
+              />
+            </View>
+
+            <MusicText
+              text={` 987438274320840932`}
+              style={[styles.textStyle, {}]}
+              preset="semiBold"
+              size="xs"
+            />
+          </Animated.View>
+        </Animated.View>
+      </Animated.View>
     )
   }
 
@@ -60,176 +166,213 @@ const Artist = () => {
     console.log("measure ===>", data)
   }
 
-  
+  const androidGradientColor = [
+    // imageColors?.average,
+    imageColors?.darkMuted,
+    imageColors?.darkVibrant,
+    // imageColors?.dominant,
+    imageColors?.lightVibrant,
+  ]
 
-  const onScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
-    console.log(event.nativeEvent.contentOffset.y)
-  }
-
-  // const animatedStyles =
-  
   return (
-    <Animated.ScrollView
-      stickyHeaderHiddenOnScroll
-      showsVerticalScrollIndicator={false}
-      onScrollBeginDrag={onScroll}
-    >
-      <View>
-        <Animated.View>{header()}</Animated.View>
-
-        {artist[0]?.data?.images[1]?.url ? (
-          <Animated.View
-            entering={FadeInLeft.delay(2000)}
-            style={{ position: "absolute", bottom: 0 }}
+    <>
+      <Animated.ScrollView
+        stickyHeaderHiddenOnScroll
+        showsVerticalScrollIndicator={false}
+        onScrollBeginDrag={onScroll}
+        scrollEventThrottle={16}
+      >
+        <Animated.View>
+          <AnimatedLinearGradient
+            colors={
+              Platform.OS === "android" ? androidGradientColor : ["#2A7B9B", "#57C785", "#EDDD53"]
+            }
+            style={[
+              styles.authorImageContainer,
+              {
+                width: DEVICE_WIDTH,
+              },
+              opacityStyle,
+            ]}
           >
-            <MusicText
-              numberOfLines={3}
-              text={artist[0]?.data?.name}
-              style={[styles.textStyle, { flexShrink: 1, justifyContent: "flex-start" }]}
-              preset="heading"
-            />
-          </Animated.View>
-        ) : null}
+            {header()}
+          </AnimatedLinearGradient>
 
-        <Pressable
-          onPress={navigateBack}
-          style={[styles.onBackNavigationContainer, { top: inset.top + 5 }]}
-        >
-          <AntDesign name="arrowleft" size={24} color="black" />
-        </Pressable>
-      </View>
+          <AnimatedPressable
+            onPress={navigateBack}
+            style={[
+              styles.onBackNavigationContainer,
+              { top: inset.top + 5 },
+              animatedPressableContainer,
+            ]}
+          >
+            <AntDesign name="arrowleft" size={24} color="black" />
+          </AnimatedPressable>
+        </Animated.View>
 
-      <View style={{ marginTop: 12 }}>
-        <View style={styles.followerContainer}>
-          <View style={styles.followingContainer}>
-            <SimpleLineIcons name="user-following" size={20} color={color.white} />
-          </View>
-
+        <View style={{ height: 100 }}>
           <MusicText
-            text={` ${artist[0]?.data?.followers?.total}`}
-            style={[styles.textStyle, {}]}
-            preset="semiBold"
-            size="xs"
+            numberOfLines={3}
+            text={artist[0]?.data?.name}
+            style={[styles.textStyle, { flexShrink: 1, justifyContent: "flex-start" }]}
+            preset="heading"
           />
         </View>
-        <View style={styles.popularityContainer}>
-          <View style={styles.popularityContainer}>
-            <AntDesign name="linechart" size={20} color={color.white} />
-          </View>
 
+        <View style={{ height: 100 }}>
           <MusicText
-            text={` ${artist[0]?.data?.popularity}`}
-            style={[styles.textStyle, {}]}
-            preset="semiBold"
-            size="xs"
+            numberOfLines={3}
+            text={artist[0]?.data?.name}
+            style={[styles.textStyle, { flexShrink: 1, justifyContent: "flex-start" }]}
+            preset="heading"
           />
         </View>
-      </View>
-      <View style={{ height: 100 }}>
-        <MusicText
-          numberOfLines={3}
-          text={artist[0]?.data?.name}
-          style={[styles.textStyle, { flexShrink: 1, justifyContent: "flex-start" }]}
-          preset="heading"
-        />
-      </View>
-      <View style={{ height: 100 }}>
-        <MusicText
-          numberOfLines={3}
-          text={artist[0]?.data?.name}
-          style={[styles.textStyle, { flexShrink: 1, justifyContent: "flex-start" }]}
-          preset="heading"
-        />
-      </View>
-      <View style={{ height: 100 }}>
-        <MusicText
-          numberOfLines={3}
-          text={artist[0]?.data?.name}
-          style={[styles.textStyle, { flexShrink: 1, justifyContent: "flex-start" }]}
-          preset="heading"
-        />
-      </View>
-      <View style={{ height: 100 }}>
-        <MusicText
-          numberOfLines={3}
-          text={artist[0]?.data?.name}
-          style={[styles.textStyle, { flexShrink: 1, justifyContent: "flex-start" }]}
-          preset="heading"
-        />
-      </View>
 
-      <View style={{ height: 100 }}>
-        <MusicText
-          numberOfLines={3}
-          text={artist[0]?.data?.name}
-          style={[styles.textStyle, { flexShrink: 1, justifyContent: "flex-start" }]}
-          preset="heading"
-        />
-      </View>
+        <View style={{ height: 100 }}>
+          <MusicText
+            numberOfLines={3}
+            text={artist[0]?.data?.name}
+            style={[styles.textStyle, { flexShrink: 1, justifyContent: "flex-start" }]}
+            preset="heading"
+          />
+        </View>
 
-      <View style={{ height: 100 }}>
-        <MusicText
-          numberOfLines={3}
-          text={artist[0]?.data?.name}
-          style={[styles.textStyle, { flexShrink: 1, justifyContent: "flex-start" }]}
-          preset="heading"
-        />
-      </View>
+        <View style={{ height: 100 }}>
+          <MusicText
+            numberOfLines={3}
+            text={artist[0]?.data?.name}
+            style={[styles.textStyle, { flexShrink: 1, justifyContent: "flex-start" }]}
+            preset="heading"
+          />
+        </View>
 
-      <View style={{ height: 100 }}>
-        <MusicText
-          numberOfLines={3}
-          text={artist[0]?.data?.name}
-          style={[styles.textStyle, { flexShrink: 1, justifyContent: "flex-start" }]}
-          preset="heading"
-        />
-      </View>
+        <View style={{ height: 100 }}>
+          <MusicText
+            numberOfLines={3}
+            text={artist[0]?.data?.name}
+            style={[styles.textStyle, { flexShrink: 1, justifyContent: "flex-start" }]}
+            preset="heading"
+          />
+        </View>
 
-      <View style={{ height: 100 }}>
-        <MusicText
-          numberOfLines={3}
-          text={artist[0]?.data?.name}
-          style={[styles.textStyle, { flexShrink: 1, justifyContent: "flex-start" }]}
-          preset="heading"
-        />
-      </View>
+        <View style={{ height: 100 }}>
+          <MusicText
+            numberOfLines={3}
+            text={artist[0]?.data?.name}
+            style={[styles.textStyle, { flexShrink: 1, justifyContent: "flex-start" }]}
+            preset="heading"
+          />
+        </View>
 
-      <View style={{ height: 100 }}>
-        <MusicText
-          numberOfLines={3}
-          text={artist[0]?.data?.name}
-          style={[styles.textStyle, { flexShrink: 1, justifyContent: "flex-start" }]}
-          preset="heading"
-        />
-      </View>
+        <View style={{ height: 100 }}>
+          <MusicText
+            numberOfLines={3}
+            text={artist[0]?.data?.name}
+            style={[styles.textStyle, { flexShrink: 1, justifyContent: "flex-start" }]}
+            preset="heading"
+          />
+        </View>
 
-      <View style={{ height: 100 }}>
-        <MusicText
-          numberOfLines={3}
-          text={artist[0]?.data?.name}
-          style={[styles.textStyle, { flexShrink: 1, justifyContent: "flex-start" }]}
-          preset="heading"
-        />
-      </View>
+        <View style={{ height: 100 }}>
+          <MusicText
+            numberOfLines={3}
+            text={artist[0]?.data?.name}
+            style={[styles.textStyle, { flexShrink: 1, justifyContent: "flex-start" }]}
+            preset="heading"
+          />
+        </View>
 
-      <View style={{ height: 100 }}>
-        <MusicText
-          numberOfLines={3}
-          text={artist[0]?.data?.name}
-          style={[styles.textStyle, { flexShrink: 1, justifyContent: "flex-start" }]}
-          preset="heading"
-        />
-      </View>
+        <View style={{ height: 100 }}>
+          <MusicText
+            numberOfLines={3}
+            text={artist[0]?.data?.name}
+            style={[styles.textStyle, { flexShrink: 1, justifyContent: "flex-start" }]}
+            preset="heading"
+          />
+        </View>
 
-      <View style={{ height: 100 }}>
-        <MusicText
-          numberOfLines={3}
-          text={artist[0]?.data?.name}
-          style={[styles.textStyle, { flexShrink: 1, justifyContent: "flex-start" }]}
-          preset="heading"
-        />
-      </View>
-    </Animated.ScrollView>
+        <View style={{ height: 100 }}>
+          <MusicText
+            numberOfLines={3}
+            text={artist[0]?.data?.name}
+            style={[styles.textStyle, { flexShrink: 1, justifyContent: "flex-start" }]}
+            preset="heading"
+          />
+        </View>
+
+        <View style={{ height: 100 }}>
+          <MusicText
+            numberOfLines={3}
+            text={artist[0]?.data?.name}
+            style={[styles.textStyle, { flexShrink: 1, justifyContent: "flex-start" }]}
+            preset="heading"
+          />
+        </View>
+
+        <View style={{ height: 100 }}>
+          <MusicText
+            numberOfLines={3}
+            text={artist[0]?.data?.name}
+            style={[styles.textStyle, { flexShrink: 1, justifyContent: "flex-start" }]}
+            preset="heading"
+          />
+        </View>
+
+        <View style={{ height: 100 }}>
+          <MusicText
+            numberOfLines={3}
+            text={artist[0]?.data?.name}
+            style={[styles.textStyle, { flexShrink: 1, justifyContent: "flex-start" }]}
+            preset="heading"
+          />
+        </View>
+
+        <View style={{ height: 100 }}>
+          <MusicText
+            numberOfLines={3}
+            text={artist[0]?.data?.name}
+            style={[styles.textStyle, { flexShrink: 1, justifyContent: "flex-start" }]}
+            preset="heading"
+          />
+        </View>
+
+        <View style={{ height: 100 }}>
+          <MusicText
+            numberOfLines={3}
+            text={artist[0]?.data?.name}
+            style={[styles.textStyle, { flexShrink: 1, justifyContent: "flex-start" }]}
+            preset="heading"
+          />
+        </View>
+
+        <View style={{ height: 100 }}>
+          <MusicText
+            numberOfLines={3}
+            text={artist[0]?.data?.name}
+            style={[styles.textStyle, { flexShrink: 1, justifyContent: "flex-start" }]}
+            preset="heading"
+          />
+        </View>
+
+        <View style={{ height: 100 }}>
+          <MusicText
+            numberOfLines={3}
+            text={artist[0]?.data?.name}
+            style={[styles.textStyle, { flexShrink: 1, justifyContent: "flex-start" }]}
+            preset="heading"
+          />
+        </View>
+
+        <View style={{ height: 100 }}>
+          <MusicText
+            numberOfLines={3}
+            text={artist[0]?.data?.name}
+            style={[styles.textStyle, { flexShrink: 1, justifyContent: "flex-start" }]}
+            preset="heading"
+          />
+        </View>
+      </Animated.ScrollView>
+    </>
   )
 }
 
@@ -237,8 +380,15 @@ export default Artist
 
 const styles = StyleSheet.create({
   authorImageStyle: {
+    alignSelf: "center",
+    borderRadius: 120,
+    height: 200,
+    width: 200,
+  },
+  authorImageContainer: {
+    alignItems: "center",
     aspectRatio: 1,
-    width: DEVICE_WIDTH,
+    justifyContent: "center",
   },
   followerContainer: {
     alignItems: "center",
